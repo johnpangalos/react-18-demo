f1c9848301204bfd00a0f848df78ab698c95bde8
John Pangalos <john@juni.co>
2022-05-07 20:43:07 +0200

    Automatic batching example

--- examples/automatic-batching-18/src/App.tsx
+++ examples/automatic-batching-18/src/App.tsx
@@ -5,12 +5,11 @@ function App() {
   const [count, setCount] = useState(0);
   useEffect(() => {
     setTimeout(() => {
-      console.time("bad idea");
+      console.time("still a bad idea");
       for (let i = 0; i < 20000; i++) {
         setCount((c) => c + 1);
       }
-
-      console.timeEnd("bad idea");
+      console.timeEnd("still a bad idea");
     }, 1000);
   }, []);
   return <div className="App">Count: {count}</div>;
