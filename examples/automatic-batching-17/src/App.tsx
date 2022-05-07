7de498a2b5ecba829af54095596813f2a088c5e0
John Pangalos <john@juni.co>
2022-05-07 21:05:39 +0200

    Refine auto batching examples

--- examples/automatic-batching-17/src/App.tsx
+++ examples/automatic-batching-17/src/App.tsx
@@ -1,19 +1,41 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [inc, setInc] = useState<number | undefined>();
+
+  function onClick() {
+    setInc((c) => {
+      if (c === undefined) return 1;
+      return c + 1;
+    });
+  }
+
   useEffect(() => {
+    if (inc === undefined) return;
     setTimeout(() => {
-      console.time("bad idea");
+      console.time("this is a bad idea");
       for (let i = 0; i < 20000; i++) {
         setCount((c) => c + 1);
       }
+      console.timeEnd("this is a bad idea");
+    }, 50);
+  }, [inc]);
 
-      console.timeEnd("bad idea");
-    }, 1000);
-  }, []);
-  return <div className="App">Count: {count}</div>;
+  return (
+    <div className="p-8 text-slate-800">
+      <div className="text-3xl font-bold pb-3">
+        React 17 Automatic Batching Example
+      </div>
+      <div className="text-xl pb-2">Count: {count}</div>
+      <button
+        className="bg-red-800 text-white px-3 py-2 rounded-lg"
+        onClick={onClick}
+      >
+        Increment
+      </button>
+    </div>
+  );
 }
 
 export default App;
